c035ec052784114e4c1fc3b12e3a78992865a702
Alessio9729 <97086582@st.deltion.nl>
2022-06-20 10:13:42 +0200

    c4

---
+++ military-assault2/Assets/Scripts/C4.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C4 : MonoBehaviour
+{
+    public float distanceToHangar;
+    public Transform hangar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(hangar.position, transform.position);
+        distanceToHangar = dist;
+
+        if(distanceToHangar <= 2)
+        {
+            if(Input.GetButtonDown("E"))
+            {
+                //Canvas.SetActive;
+            }
+        }
+        
+    }
+}
